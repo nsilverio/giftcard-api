31ee9fa554d6e2ac7ca14802924bfb104efcff68
nataliwt <natali@weatlhtab.com>
2020-03-30 14:16:47 +0100

    Add Mongoose validation error handlers

--- controllers/merchants.js
+++ controllers/merchants.js
@@ -7,11 +7,16 @@ const ErrorResponse = require('../utils/errorResponse')
 exports.getMerchants = async (req, res, next) => {
     try {
         const merchants = await Merchant.find()
-        res.status(200).json({ success: true, count: merchants.length, data: merchants})
+
+        res.status(200).json({ success: true, count: merchants.length, data: merchants })
     } catch (error) {
-        res.status(400).json({success: false })
+        /* 
+       pass the error to Express default error handler 
+       see more https://expressjs.com/en/guide/error-handling.html
+       */
+        next(error)
     }
-} 
+}
 
 // @desc    Get a single Merchant
 // @route   GET /api/v1/merchants/:id
@@ -19,15 +24,15 @@ exports.getMerchants = async (req, res, next) => {
 exports.getMerchant = async (req, res, next) => {
     try {
         const merchant = await Merchant.findById(req.params.id)
-        
-        if(!merchant)
+
+        if (!merchant)
             return next(new ErrorResponse(`Merchant not found with id of ${req.params.id}`, 404))
 
-        res.status(200).json({ success: true, data: merchant})
+        res.status(200).json({ success: true, data: merchant })
     } catch (error) {
-        next(new ErrorResponse(`Merchant not found with id of ${req.params.id}`, 404))
+        next(error)
     }
-} 
+}
 
 // @desc    Create new Merchant
 // @route   POST /api/v1/merchants
@@ -36,14 +41,18 @@ exports.createMerchant = async (req, res, next) => {
     try {
         const merchant = await Merchant.create(req.body)
 
+        // Corrected formated object ID that does not exist in the database
+        if (!merchant)
+            res.status(400).json({ success: false })
+
         res.status(201).json({
             success: true,
             data: merchant
         })
     } catch (error) {
-      res.status(400).json( {success: false })  
+        next(error)
     }
-} 
+}
 
 // @desc    Update a Merchant
 // @route   PUT /api/v1/merchants/:id
@@ -55,17 +64,17 @@ exports.updateMerchant = async (req, res, next) => {
             runValidators: true
         })
 
-        if(!merchant)
-            res.status(400).json( {success: false })    
-        
+        if (!merchant)
+            return next(new ErrorResponse(`Merchant not found with id of ${req.params.id}`, 404))
+
         res.status(201).json({
             success: true,
             data: merchant
         })
     } catch (error) {
-      res.status(400).json( {success: false })  
+        next(error)
     }
-} 
+}
 
 // @desc    Delete a Merchant
 // @route   GET /api/v1/merchants/:id
@@ -74,14 +83,14 @@ exports.deleteMerchant = async (req, res, next) => {
     try {
         const merchant = await Merchant.findByIdAndDelete(req.params.id)
 
-        if(!merchant)
-            res.status(400).json( {success: false })    
-        
+        if (!merchant)
+            return next(new ErrorResponse(`Merchant not found with id of ${req.params.id}`, 404))
+
         res.status(201).json({
             success: true,
             data: {}
         })
     } catch (error) {
-      res.status(400).json( {success: false })  
+        next(error)
     }
 } 
--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,34 @@
-const errorHandler = (err, req, res, next ) => {
-    // log to console to dev
-    console.log(err.stack.red)
+const ErrorResponse = require('../utils/errorResponse')
 
-    res.status(500).json({ 
-        success:false, 
-        error: err.message
+const errorHandler = (err, req, res, next) => {
+
+    let error = { ...err }
+    error.message = err.message
+
+    // log to console to dev
+    console.log(err)
+
+    // Mongoose bad ObjectID
+    if (err.name === 'CastError') {
+        const message = `Resource not found with id ${err.value}`
+        error = new ErrorResponse(message, 404)
+    }
+
+    // Mongoose duplicated field entered
+    if (err.code === 11000) {
+        const message = `Duplicated field entered`
+        error = new ErrorResponse(message, 404)
+    }
+
+    // Mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message)
+        error = new ErrorResponse(message, 404)
+    }
+
+    res.status(error.statusCode || 500).json({
+        success: false,
+        error: error.message || 'Server error'
     })
 }
 module.exports = errorHandler

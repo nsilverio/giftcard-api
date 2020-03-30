07cb421fc01da83f14ef878e8c60e30627d9ae75
nataliwt <natali@weatlhtab.com>
2020-03-30 16:05:43 +0100

    Remove try/catch blocks from controller, add asyncHlander

--- controllers/merchants.js
+++ controllers/merchants.js
@@ -1,96 +1,76 @@
 const Merchant = require('../models/Merchant')
 const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
 
 // @desc    Get all merchants
 // @route   GET /api/v1/merchants
 // @access  Public
-exports.getMerchants = async (req, res, next) => {
-    try {
-        const merchants = await Merchant.find()
+exports.getMerchants = asyncHandler(async (req, res, next) => {
+    const merchants = await Merchant.find()
 
-        res.status(200).json({ success: true, count: merchants.length, data: merchants })
-    } catch (error) {
-        /* 
-       pass the error to Express default error handler 
-       see more https://expressjs.com/en/guide/error-handling.html
-       */
-        next(error)
-    }
-}
+    res.status(200).json({ success: true, count: merchants.length, data: merchants })
+})
 
 // @desc    Get a single Merchant
 // @route   GET /api/v1/merchants/:id
 // @access  Public
-exports.getMerchant = async (req, res, next) => {
-    try {
-        const merchant = await Merchant.findById(req.params.id)
+exports.getMerchant = asyncHandler(async (req, res, next) => {
+    const merchant = await Merchant.findById(req.params.id)
 
-        if (!merchant)
-            return next(new ErrorResponse(`Merchant not found with id of ${req.params.id}`, 404))
+    if (!merchant)
+        return next(new ErrorResponse(`Merchant not found with id of ${req.params.id}`, 404))
 
-        res.status(200).json({ success: true, data: merchant })
-    } catch (error) {
-        next(error)
-    }
-}
+    res.status(200).json({ success: true, data: merchant })
+
+})
 
 // @desc    Create new Merchant
 // @route   POST /api/v1/merchants
 // @access  Private
-exports.createMerchant = async (req, res, next) => {
-    try {
-        const merchant = await Merchant.create(req.body)
+exports.createMerchant = asyncHandler(async (req, res, next) => {
+    const merchant = await Merchant.create(req.body)
 
-        // Corrected formated object ID that does not exist in the database
-        if (!merchant)
-            res.status(400).json({ success: false })
+    // Corrected formated object ID that does not exist in the database
+    if (!merchant)
+        res.status(400).json({ success: false })
 
-        res.status(201).json({
-            success: true,
-            data: merchant
-        })
-    } catch (error) {
-        next(error)
-    }
-}
+    res.status(201).json({
+        success: true,
+        data: merchant
+    })
+})
 
 // @desc    Update a Merchant
 // @route   PUT /api/v1/merchants/:id
 // @access  Private
-exports.updateMerchant = async (req, res, next) => {
-    try {
-        const merchant = await Merchant.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
+exports.updateMerchant = asyncHandler(async (req, res, next) => {
 
-        if (!merchant)
-            return next(new ErrorResponse(`Merchant not found with id of ${req.params.id}`, 404))
+    const merchant = await Merchant.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
 
-        res.status(201).json({
-            success: true,
-            data: merchant
-        })
-    } catch (error) {
-        next(error)
-    }
-}
+    if (!merchant)
+        return next(new ErrorResponse(`Merchant not found with id of ${req.params.id}`, 404))
+
+    res.status(201).json({
+        success: true,
+        data: merchant
+    })
+})
 
 // @desc    Delete a Merchant
 // @route   GET /api/v1/merchants/:id
 // @access  private
-exports.deleteMerchant = async (req, res, next) => {
-    try {
-        const merchant = await Merchant.findByIdAndDelete(req.params.id)
+exports.deleteMerchant = asyncHandler(async (req, res, next) => {
+    const merchant = await Merchant.findByIdAndDelete(req.params.id)
 
-        if (!merchant)
-            return next(new ErrorResponse(`Merchant not found with id of ${req.params.id}`, 404))
+    if (!merchant)
+        return next(new ErrorResponse(`Merchant not found with id of ${req.params.id}`, 404))
 
-        res.status(201).json({
-            success: true,
-            data: {}
-        })
-    } catch (error) {
-        next(error)
-    }
-} 
+    res.status(201).json({
+        success: true,
+        data: {}
+    })
+
+})

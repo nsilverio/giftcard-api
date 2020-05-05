ccc05db300d8e0bd8e7c783510423a47ff14d089
nataliwt <natali@weatlhtab.com>
2020-05-05 20:31:49 +0100

    add logout

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express')
 const {
     register,
     login,
+    logout,
     getMe,
     forgotPassword,
     resetPassword,
@@ -17,6 +18,7 @@ const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
+router.get('/logout', logout)
 router.get('/me', protect, getMe)
 router.post('/forgotpassword', forgotPassword)
 router.put('/resetpassword/:resettoken', resetPassword)
